090264246d253ea158eab47f40a82a9501df98a7
Jae Hyung Kim <SC425@ypsa.com>
2024-10-29 10:34:55 +0600

    스트라이프 제거

--- app/test/loading.tsx
+++
@@ -1,3 +0,0 @@
-export default function Loading() {
-  return <h1>로딩1페이지</h1>;
-}
--- components/user-dashboard/reservations/user-Reservations.tsx
+++ components/user-dashboard/reservations/user-Reservations.tsx
@@ -1,6 +1,6 @@
 "use client";
 
-import { useEffect, useState } from "react";
+import { useEffect } from "react";
 import { Button } from "../../ui/button";
 import { DateTime } from "luxon";
 import UserReservationList from "./user-reservation-list";
